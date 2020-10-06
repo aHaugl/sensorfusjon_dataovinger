210875f5bd40d9c743721d7eb769770b4d87ecf1
Emil Laptop <emil.martens@gmail.com>
2020-10-06 15:00:07 +0200

    task1a working

--- task4/perform_mixture_reduction.py
+++ task4/perform_mixture_reduction.py
@@ -1,13 +1,15 @@
 # %% imports
 import numpy as np
+import matplotlib
 import matplotlib.pyplot as plt
 from scipy.stats import multivariate_normal
 from mixturereduction import gaussian_mixture_moments
+matplotlib.use('Qt5Cairo')
 
 # %% setup and show initial
 # TODO: fill in values
-mus = np.array([1, 1, 1]).reshape(3, 1)
-sigmas = np.array([1, 1, 1]).reshape(
+mus = np.array([1, 2, 3]).reshape(3, 1)
+sigmas = np.array([1, 1, 2]).reshape(
     3, 1, 1
 )  # note std and not var as in gaussian_mixture_moments
 w = np.array([1, 1, 1])
@@ -23,7 +25,8 @@ x = totMean + plotNsigmas * np.sqrt(totSigma2) * np.arange(-1, 1 + 1e-10, 5e-2)
 fig1, ax1 = plt.subplots(num=1, clear=True)
 pdf_comp_vals = np.array(
     [
-        multivariate_normal.pdf(x, mean=mus[i].item(), cov=sigmas[i].item() ** 2)
+        multivariate_normal.pdf(
+            x, mean=mus[i].item(), cov=sigmas[i].item() ** 2)
         for i in range(len(mus))
     ]
 )
@@ -44,7 +47,8 @@ sigma2comb = np.zeros_like(w)
 pdf_mix_comb_vals = np.zeros_like(pdf_comp_vals)
 for i in range(2):  # index of first to merge
     for j in range(i + 1, 3):  # index of second to merge
-        k_other = 2 - k  # the index of the non merging (only works for 3 components)
+        # the index of the non merging (only works for 3 components)
+        k_other = 2 - k
 
         # merge components
         wcomb[k] = w[i] + w[j]
@@ -60,10 +64,11 @@ for i in range(2):  # index of first to merge
         ax2.plot(x, pdf_mix_comb_vals[k], label=f"combining {i} {j}")
         k += 1
 
-ax2.legend(loc=(1.05, 0))
+ax2.legend()
 
 print(mucomb)
 print(sigma2comb)
 sigmacomb = np.sqrt(sigma2comb)
 
 # %%
+plt.show()
